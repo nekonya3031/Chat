bb26f03902d96c27b82fdbc1686ee3417fb7891c
Misaka <misakachan2545@gmail.com>
2021-02-22 12:15:37 +0300

    Град и гитигнор

--- chat/function/Area.java
+++
@@ -1,8 +0,0 @@
-package chat.function;
-
-public class Area{
-	public static int x, y;
-	public Area(int x, int y){
-		this.x = x; this.y = y;
-	}
-}
---
+++ src/main/java/chat/function/Area.java
@@ -0,0 +1,8 @@
+package chat.function;
+
+public class Area{
+	public static int x, y;
+	public Area(int x, int y){
+		this.x = x; this.y = y;
+	}
+}
