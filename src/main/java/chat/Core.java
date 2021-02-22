419128af6d03f0bbf501d3fb73215d8a45a03ae4
Misaka <misakachan2545@gmail.com>
2021-02-22 16:27:01 +0300

    Смена порта

--- src/main/java/chat/Core.java
+++ src/main/java/chat/Core.java
@@ -6,6 +6,6 @@ public class Core{
     public static String ipAddr = "shizashizashiza.ml";
     public static Area size = new Area(700, 560);
     public static Version version = new Version("interface update", "pre-alpha", 2.34f);
-    public static int serverPORT = 8080;
-    public static int port = 8080;
+    public static int serverPORT = 9000;
+    public static int port = 9000;
 }
