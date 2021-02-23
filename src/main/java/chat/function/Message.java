9d7e1360ccc517b11bed66e00ce3fe86c5175afb
Misaka <misakachan2545@gmail.com>
2021-02-23 17:18:38 +0300

    Создание грузовго класса

---
+++ src/main/java/chat/function/Message.java
@@ -0,0 +1,40 @@
+package chat.function;
+
+import java.util.Date;
+
+public class Message {
+    Long id;
+    String text;
+    String name;
+    Date date;
+    int type;
+
+    public Message(Long id, String text, String name, int type) {
+        this.id = id;
+        this.text = text;
+        this.name = name;
+        this.type = type;
+        this.date = new Date();
+    }
+
+    public Message(Long id, String text, String name, int type, Date date) {
+        this.id = id;
+        this.text = text;
+        this.name = name;
+        this.type = type;
+        this.date = date;
+    }
+
+    public Message(String json) {
+        Message msg = Version.gson.fromJson(json, Message.class);
+        this.type = msg.type;
+        this.name = msg.name;
+        this.date = msg.date;
+        this.id = msg.id;
+        this.text = msg.text;
+    }
+
+    public String toSend() {
+        return Version.gson.toJson(this);
+    }
+}
--- src/main/java/chat/function/Version.java
+++ src/main/java/chat/function/Version.java
@@ -1,11 +1,16 @@
 package chat.function;
 
+import com.google.gson.Gson;
+
 public class Version {
     public static String prefix, type;
     public static float version;
-    public Version(String prefix, String type, float version){
+    public static Gson gson;
+
+    public Version(String prefix, String type, float version) {
         this.version = version;
         this.prefix = prefix;
         this.type = type;
+        this.gson = new Gson();
     }
 }
